ed4fd35e8fba0db86b47b39ba320f6ae74699029
Valery Semenenko <gearmobile@gmail.com>
2025-09-15 14:25:58 +0300

    add switch

--- main.go
+++ main.go
@@ -3,32 +3,22 @@ package main
 import (
 	"fmt"
 	"math"
+	"strings"
 )
 
 func main() {
-
 	fmt.Println("__Калькулятор индекса массы тела__")
 
-	height, weight := getUserInputs()
-	imt := calculateImt(height, weight)
+	for {
+		height, weight := getUserInputs()
+		imt := calculateImt(height, weight)
 
-	if imt < 16 {
-		fmt.Println("У вас сильный дефицит массы тела")
-	} else if imt >= 16 && imt < 18.5 {
-		fmt.Println("У вас дефицит массы тела")
-	} else if imt >= 18.5 && imt < 25 {
-		fmt.Println("У вас нормальный индекс массы тела")
-	} else if imt >= 25 && imt < 30 {
-		fmt.Println("У вас избыточная масса тела")
-	} else if imt >= 30 && imt < 35 {
-		fmt.Println("У вас 1-я степень ожирения")
-	} else if imt >= 35 && imt < 40 {
-		fmt.Println("У вас 2-я степень ожирения")
-	} else {
-		fmt.Println("У вас 3-я степень ожирения")
+		outputResult(imt)
+
+		if !checkUserAction() {
+			break
+		}
 	}
-
-	outputResult(imt)
 }
 
 func getUserInputs() (height float64, weight uint8) {
@@ -48,5 +38,31 @@ func calculateImt(height float64, weight uint8) float64 {
 }
 
 func outputResult(imt float64) {
-	fmt.Printf("Ваш индекс массы тела равен: %.0f", imt)
+	message := fmt.Sprintf("Ваш индекс массы тела равен: %.0f", imt)
+	fmt.Println(message)
+
+	switch {
+	case imt < 16:
+		fmt.Println("У вас сильный дефицит массы тела")
+	case imt < 18.5:
+		fmt.Println("У вас дефицит массы тела")
+	case imt < 25:
+		fmt.Println("У вас нормальный индекс массы тела")
+	case imt < 30:
+		fmt.Println("У вас избыточная масса тела")
+	case imt < 35:
+		fmt.Println("У вас 1-я степень ожирения")
+	case imt < 40:
+		fmt.Println("У вас 2-я степень ожирения")
+	default:
+		fmt.Println("У вас 3-я степень ожирения")
+	}
+}
+
+func checkUserAction() bool {
+	userAction := "y"
+	fmt.Print("Хотите продолжить (y/n)?")
+	fmt.Scan(&userAction)
+
+	return strings.ToLower(userAction) == "y" || strings.ToLower(userAction) == "yes"
 }
